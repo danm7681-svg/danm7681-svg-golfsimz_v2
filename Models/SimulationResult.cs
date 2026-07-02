dcc2776f7bacefd356c822bdd82f6a726e33d691
danm7681-svg <danm7681@gmail.com>
2026-07-02 14:43:52 +0100

    Add SimulationResult class with properties

--- Models/SimulationResult.cs
+++ Models/SimulationResult.cs
@@ -1,1 +1,12 @@
+namespace golfsimz_v2.Models;
 
+public class SimulationResult
+{
+    public float CarryYards { get; set; }
+    public float TotalYards { get; set; }
+    public float ApexFeet { get; set; }
+    public float HangTimeSeconds { get; set; }
+    public float OfflineYards { get; set; }
+    public float LandingAngleDeg { get; set; }
+    public List<TrajectoryPoint> Trajectory { get; set; } = new();
+}
