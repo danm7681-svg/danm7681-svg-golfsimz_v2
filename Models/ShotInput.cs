a155ced194999bb94bbeea31320317e5f2364cb4
danm7681-svg <danm7681@gmail.com>
2026-07-02 14:43:01 +0100

    Add ShotInput class with default properties

--- Models/ShotInput.cs
+++ Models/ShotInput.cs
@@ -1,1 +1,9 @@
+namespace golfsimz_v2.Models;
 
+public class ShotInput
+{
+    public float BallSpeedMph { get; set; } = 150f;
+    public float LaunchAngleDeg { get; set; } = 12f;
+    public float SideAngleDeg { get; set; } = 0f;
+    public float BackspinRpm { get; set; } = 2500f;
+}
