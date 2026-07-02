0834f2037e59d8dc058a6d9818301d963a8b7f36
danm7681-svg <danm7681@gmail.com>
2026-07-02 14:44:05 +0100

    Update TrajectoryPoint.cs

--- Models/TrajectoryPoint.cs
+++ Models/TrajectoryPoint.cs
@@ -1,1 +1,9 @@
+namespace golfsimz_v2.Models;
 
+public class TrajectoryPoint
+{
+    public float Time { get; set; }
+    public float DownrangeYards { get; set; }
+    public float AltitudeFeet { get; set; }
+    public float OfflineYards { get; set; }
+}
